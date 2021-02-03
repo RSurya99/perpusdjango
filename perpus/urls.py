9dc37f0854887b0faac7d0fb7e1228825dd63472
RSurya99 <Rafliazz679@gmail.com>
2021-02-03 12:20:12 +0700

    pembuatan rest api

--- perpus/urls.py
+++ perpus/urls.py
@@ -14,12 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from perpustakaan.views import *
 from django.contrib.auth.views import LoginView, LogoutView
 from django.conf.urls.static import static
+from perpustakaan.viewset_api import *
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('kelompok', KelompokViewset)
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
     path('buku/', buku, name='buku'),
     path('penerbit/', penerbit, name='penerbit'),
